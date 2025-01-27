b1569533c53675bbe72b8c20b54cceb5f8c78b4d
debianbaker <linuxrocker002@gmail.com>
2025-01-28 00:19:23 +0530

    Added Cards from Aceternity and made Dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CardBody, CardContainer, CardItem } from "@/components/global/3d-card";
 import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
@@ -5,6 +6,7 @@ import { LampComponent, LampContainer } from "@/components/global/lamp";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
 import { clients, products } from "@/lib/constants";
+import { CheckIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -61,6 +63,152 @@ export default function Home() {
         </section>
         <section className="mt-[-500px]">
           <LampComponent/>
+          <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
+            <CardContainer className ="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl
+               dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]
+               w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+                <CardItem
+                  translateZ="50" 
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    Hobby
+                    <h2 className="text-6xl">$0</h2>
+                </CardItem>
+                <CardItem
+                  translateZ="60" 
+                  className="text-sm max-w-sm mt-2 text-neutral-600 dark:text-neutral-100"
+                  >
+                    Get a glimpse of what our software is capable of. Just a heads up {"you'll "}  
+                    never leave us after this! 
+                    <ul className="my-4 flex flex-col gap-2">
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 Free Automations
+                      </li>
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 100 tasks per month
+                      </li>
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 Two-step Actions
+                      </li>
+                    </ul>
+                </CardItem>
+                <div className="flex justify-between items-center mt-8">
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try Now →
+                  </CardItem>
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-bold text-white
+                     dark:text-black dark:bg-white"
+                  >
+                    Get Started Now
+                  </CardItem>
+                </div>
+               </CardBody>
+            </CardContainer>
+            <CardContainer className ="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl
+               dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-[#E2CBFF] border-black/[0.1]
+               w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+                <CardItem
+                  translateZ="50" 
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    Pro Plan
+                    <h2 className="text-6xl">$19</h2>
+                </CardItem>
+                <CardItem
+                  translateZ="60" 
+                  className="text-sm max-w-sm mt-2 text-neutral-600 dark:text-neutral-100"
+                  >
+                    Get a glimpse of what our software is capable of. Just a heads up {"you'll "}  
+                    never leave us after this! 
+                    <ul className="my-4 flex flex-col gap-2">
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 Free Automations
+                      </li>
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 100 tasks per month
+                      </li>
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 Two-step Actions
+                      </li>
+                    </ul>
+                </CardItem>
+                <div className="flex justify-between items-center mt-8">
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try Now →
+                  </CardItem>
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-bold text-white
+                     dark:text-black dark:bg-white"
+                  >
+                    Get Started Now
+                  </CardItem>
+                </div>
+               </CardBody>
+            </CardContainer>
+            <CardContainer className ="inter-var">
+              <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl
+               dark:hover:shadow-neutral-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]
+               w-full md:!w-[350px] h-auto rounded-xl p-6 border">
+                <CardItem
+                  translateZ="50" 
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                  >
+                    Unlimited
+                    <h2 className="text-6xl">$89</h2>
+                </CardItem>
+                <CardItem
+                  translateZ="60" 
+                  className="text-sm max-w-sm mt-2 text-neutral-600 dark:text-neutral-100"
+                  >
+                    Get a glimpse of what our software is capable of. Just a heads up {"you'll "}  
+                    never leave us after this! 
+                    <ul className="my-4 flex flex-col gap-2">
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 Free Automations
+                      </li>
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 100 tasks per month
+                      </li>
+                      <li className="flex items-center gap-2">
+                        <CheckIcon/>3 Two-step Actions
+                      </li>
+                    </ul>
+                </CardItem>
+                <div className="flex justify-between items-center mt-8">
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try Now →
+                  </CardItem>
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl text-xs font-bold text-white
+                     dark:text-black dark:bg-white"
+                  >
+                    Get Started Now
+                  </CardItem>
+                </div>
+               </CardBody>
+            </CardContainer>
+          </div>
         </section>
       </main>
   );
