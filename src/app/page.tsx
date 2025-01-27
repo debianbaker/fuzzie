2adaf2017175fdc9aea0aec159f6c8af5a4e63fb
debianbaker <linuxrocker002@gmail.com>
2025-01-27 23:00:11 +0530

    Added HeroParallax, Lamp Sparkles from Aceternity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
+import { LampComponent, LampContainer } from "@/components/global/lamp";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
-import { clients } from "@/lib/constants";
+import { clients, products } from "@/lib/constants";
 import Image from "next/image";
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main>
         <Navbar/>
         <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative 
         flex flex-col items-center antialiased">
@@ -19,38 +21,47 @@ export default function Home() {
             <ContainerScroll 
              titleComponent = 
               {
-                <div className="flex items-center justify-center flex-col">
+                <div className="flex items-center flex-col">
                   <Button 
                     size={'lg'}
                     className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit 
                     border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] 
                     hover:bg-white group transition-all flex items-center
                     justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 
-                    duration:-500">
-                      <span className="bg-clip-text text-transparent bg-gradient-to-r
+                    duration-500"
+                  >
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r
                       from-neutral-500 to-neutral-600 md:text-center font-sans
                       group-hover:bg-gradient-to-r group-hover:from-black
                       group-hover:to-black">
                         Start for Free Today
-                      </span>
-                    </Button>
-                    <h1 className ="text-5xl md:text-8xl bg-clip-text text-transparent  
+                    </span>
+                  </Button>
+                  <h1 className ="text-5xl md:text-8xl bg-clip-text text-transparent  
                     bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
                       Automate Your Work With Fuzzie 
-                    </h1>
+                  </h1>
                 </div>
               }
               ></ContainerScroll>
            </div>
         </section>
         <InfiniteMovingCards
-          className="md:mt-[18rem] mt-[-100px]"
+          className="md:mt-[28rem] mt-[65px]"
           items={clients}  
           direction="right"
           speed="slow"
         />
         {// clients is the Array of hrefs
           }
+        <section>
+          <HeroParallax 
+            products = {products}
+          ></HeroParallax>
+        </section>
+        <section className="mt-[-500px]">
+          <LampComponent/>
+        </section>
       </main>
   );
 }
