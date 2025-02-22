a10b978b36dc88bb4376f2fa6bdb78ac20e58206
debianbaker <linuxrocker002@gmail.com>
2025-02-22 19:37:46 +0530

    Wrokflow: Modal Context, drawer-shadcn, making form

---
+++ src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import CustomModal from "@/components/global/custom-modal"
+import { Button } from "@/components/ui/button"
+import { useModal } from "@/providers/modal-provider"
+import { Plus } from "lucide-react"
+
+type Props = {}
+
+const WorkflowButton = (props: Props) => {
+    
+    const {setOpen, setClose} = useModal()
+    const handleClick = () => {
+        setOpen(
+            <CustomModal
+                title="Create a Workflow Automation"
+                subheading="Workflows are a powerful tool that help you automate tasks."
+            >
+                 <Workflowform/>
+            </CustomModal>
+        )
+    }
+    return (
+        <Button 
+            className={'icon'} 
+            onClick={handleClick}>
+            <Plus/>
+        </Button>
+    )
+}
+export default WorkflowButton
